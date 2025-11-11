d1718f2110e281560091094d2f02a7f814740410
edervega08 <edervb2000@gmail.com>
2025-11-10 23:27:10 -0600

    1

--- Basic/00-helloworld.js
+++ Basic/00-helloworld.js
@@ -1,10 +1,3 @@
-/*
-Clases 1 a 14 (sin código)
-Vídeo: https://youtu.be/1glVfFxj8a4
-
-Clase 15 - Hola mundo
-Vídeo: https://youtu.be/1glVfFxj8a4?t=2390
-*/
 
 // Esto es un comentario simple
 
@@ -25,12 +18,22 @@ console.log("¡Hola, JavaScript!")
 console.log('¡Hola, JavaScript!')
 console.log(`¡Hola, JavaScript!`)
 */
+console.log("5");          // String (texto entre comillas)
+console.log(5);            // Número literal
 
-console.log("5")
-console.log(5)
-console.log(5 + 2)
-console.log(5 - 2)
-console.log(5 * 2)
-console.log(5 / 2)
-console.log(5 % 2)
-console.log(5 ** 2)
+console.log(5 + 2);        // Operador de suma (+): suma números
+console.log("Hola " + "Eder"); // Operador de concatenación (+): une textos
+
+console.log(5 - 2);        // Operador de resta (-): resta valores numéricos
+console.log(5 * 2);        // Operador de multiplicación (*): multiplica valores
+console.log(5 / 2);        // Operador de división (/): divide valores
+
+console.log(5 % 2);        // Operador módulo (%): devuelve el residuo de la división (5 ÷ 2 = 2, sobra 1)
+console.log(10 % 3);       // Módulo (%): residuo de 10 ÷ 3 (sobra 1)
+//🧠 Muy usado para saber si un número es par o impar:
+//if (x % 2 === 0) console.log("Es par");
+
+// Operador de exponenciación
+console.log(5 ** 2); // 25 (5 al cuadrado)
+console.log(2 ** 3); // 8  (2 al cubo)
+
